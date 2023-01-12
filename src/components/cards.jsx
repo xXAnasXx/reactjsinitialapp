680a3b3481f61cccf722aab431341d66fabc4011
xXAnasXx <anass-ab-@hotmail.com>
2023-01-12 13:15:10 -0500

    header and cards done

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -5,6 +5,11 @@ import { useState, useEffect } from 'react';
 const Cards = () => {
 
     const [cases, setCases] = useState([]);
+    const [deaths, setDeaths] = useState([]);
+    const [hospitalizations, setHospitalizations] = useState([]);
+    const [vaccine_coverage_dose_1, setVaccine_coverage_dose_1] = useState([])
+    const [vaccine_coverage_dose_2, setVaccine_coverage_dose_2] = useState([])
+    const [vaccine_coverage_dose_3, setVaccine_coverage_dose_3] = useState([])
 
     useEffect(() => {
         fetch('https://api.opencovid.ca/timeseries?stat=cases&date=1&geo=can')
@@ -18,57 +23,120 @@ const Cards = () => {
             });
     }, []);
 
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/timeseries?stat=deaths&date=1&geo=can')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setDeaths(data.data.deaths);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/timeseries?stat=hospitalizations&date=1&geo=can')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setHospitalizations(data.data.hospitalizations);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/timeseries?stat=vaccine_coverage_dose_1&date=1&geo=can')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setVaccine_coverage_dose_1(data.data.vaccine_coverage_dose_1);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/timeseries?stat=vaccine_coverage_dose_2&date=1&geo=can')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setVaccine_coverage_dose_2(data.data.vaccine_coverage_dose_2);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/timeseries?stat=vaccine_coverage_dose_3&date=1&geo=can')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setVaccine_coverage_dose_3(data.data.vaccine_coverage_dose_3);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+    
+    function errorHandling(val){
+        try{
+            return(
+                <i>{val.value}</i>
+            )
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
     return (
         <div className="Body container">
             <div className='row gx-5'>
                 <div className='col-sm'>
                     <div className='card bg-danger text-white ml-3 cardS'>
                         Cases : {
-                            cases == undefined ? <i>None</i> : <i>{cases[0].value}</i>
+                            errorHandling(cases[0])
                         }
                     </div>
                 </div>
                 <div className='col-sm'>
                     <div className='card bg-dark text-white ml-3 cardS'>
-                        Deaths
+                        Deaths : { errorHandling(deaths[0]) }
                     </div>
                 </div>
                 <div className='col-sm'>
                     <div className='card bg-success text-white ml-3 cardS'>
-                        Hospitalization
+                        Hospitalization : {
+                            errorHandling(hospitalizations[0])
+                        }
                     </div>
                 </div>
                 <div className='col-sm'>
                     <div className='card bg-light ml-3 cardL'>
-                        Vaccine coverage dose 1
+                        Vaccine coverage dose 1 : {
+                            errorHandling(vaccine_coverage_dose_1[0])
+                        }
                     </div>
                 </div>
                 <div className='col-sm'>
                     <div className="card bg-light ml-3 cardL">
-                        Vaccine coverage dose 2
+                        Vaccine coverage dose 2 : {
+                            errorHandling(vaccine_coverage_dose_2[0])
+                        }
                     </div>
                 </div>
                 <div className='col-sm'>
                     <div className="card bg-light ml-3 cardL">
-                        Vaccine coverage dose 3
+                        Vaccine coverage dose 3 : {
+                            errorHandling(vaccine_coverage_dose_3[0])
+                        }
                     </div>
                 </div>
             </div>
-            {/*             <div className="posts-container">
-                {
-                posts.map((post) => {
-                    return (
-                        <div className="post-card" key={post.region}>
-                        <h2 className="post-title">{post.cases}</h2>
-                        <p className="post-body">{post.deaths}</p>
-                        <div className="button">
-                        <div className="delete-btn">Delete</div>
-                        </div>
-                        </div>
-                    );
-                })
-                }
-            </div> */}
         </div>
 
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,26 @@
 import '../style/customisedStyle.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 
 const Header = () => {
-    const [date, setDate] = useState("");
+    const [date, setDate] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.opencovid.ca/version')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setDate(data.summary);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
     return(
         <div className="Header row justify-content-between">
             <div className="col-10">
-                <i>Date : </i>
-                <i>{date}</i>
+                <i>Date : {date} </i>
             </div>
             <div className="col-2">
                 <i>Country : </i>
