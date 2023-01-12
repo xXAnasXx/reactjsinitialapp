d0852f4d6869e25274fcc8055d6c2321e25d8277
xXAnasXx <anass-ab-@hotmail.com>
2023-01-12 08:18:10 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 //import { useState } from 'react'
-import './App.css'
-import Header from './header'
-
+//import './App.css'
+import Header from './components/header'
+import Cards from './components/cards'
 function App() {
   //const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className='m-4'>
       <Header />
+      <Cards />
     </div>
+      
+    
   )
 }
 
