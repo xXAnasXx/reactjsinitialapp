6a1270b179d0ad694ab3844652e59eb75b09a961
xXAnasXx <anass-ab-@hotmail.com>
2023-01-16 09:41:17 -0500

    'react-table-integration-first'

--- src/components/arrayInfos.jsx
+++ src/components/arrayInfos.jsx
@@ -1,11 +1,91 @@
 import '../style/customisedStyle.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { render } from 'react-dom';
+import { useTable } from 'react-table'
 
 const ArrayInfos = () => {
     const [covidData, setCovidData] = useState([]);
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'Region',
+                accessor: 'region', // the 'key'
+            },
+            {
+                Header: 'Cases',
+                accessor: 'cases_daily'
+            },
+            {
+                Header: 'Deaths',
+                accessor: 'deaths'
+            },
+            {
+                Header: 'Deaths Daily',
+                accessor: 'deaths_daily'
+            },
+            {
+                Header: 'Hospitalizations',
+                accessor: 'hospitalizations'
+            },
+            {
+                Header: 'Hospitalizations Daily',
+                accessor: 'hospitalizations_daily'
+            },
+            {
+                Header: 'ICU',
+                accessor: 'icu'
+            },
+            {
+                Header: 'ICU Daily',
+                accessor: 'icu_daily'
+            },
+            {
+                Header: 'Tests Completed',
+                accessor: 'tests_completed'
+            },
+            {
+                Header: 'Tests Completed Daily',
+                accessor: 'tests_completed_daily'
+            },
+            {
+                Header: 'VC dose 1',
+                accessor: 'vc_dose_1'
+            },
+            {
+                Header: 'VC dose 1 daily',
+                accessor: 'vc_dose_1_daily'
+            },
+            {
+                Header: 'VC dose 2',
+                accessor: 'vc_dose_2'
+            },
+            {
+                Header: 'VC dose 2 daily',
+                accessor: 'vc_dose_2_daily'
+            },
+            {
+                Header: 'VC dose 3',
+                accessor: 'vc_dose_3'
+            },
+            {
+                Header: 'VC dose 3 daily',
+                accessor: 'vc_dose_3_daily'
+            },
+        ]
+    )
 
+    
+    const tableInstance = covidData == undefined ? [] : useTable({ columns, covidData });
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance
+    
     useEffect(() => {
         fetch('https://api.opencovid.ca/summary')
             .then((response) => response.json())
@@ -18,57 +98,54 @@ const ArrayInfos = () => {
             });
     }, []);
 
-    return(
+    return (
         <div className='mt-5'>
-            <table className='table table-sm table-striped'>
+            <table {...getTableProps()} className='table table-sm table-striped'>
                 <thead>
-                    <tr>
-                        <th>Region</th>
-                        <th>Cases</th>
-                        <th>Cases Daily</th>
-                        <th>Deaths</th>
-                        <th>Deaths Daily</th>
-                        <th>Hospitalizations</th>
-                        <th>Hospitalizations Daily</th>
-                        <th>ICU</th>
-                        <th>ICU Daily</th>
-                        <th>Tests Completed</th>
-                        <th>Tests Completed Daily</th>
-                        <th>VC dose 1</th>
-                        <th>VC dose 1 daily</th>
-                        <th>VC dose 2</th>
-                        <th>VC dose 2 daily</th>
-                        <th>VC dose 3</th>
-                        <th>VC dose 3 daily</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {covidData.map((elem) => {
-                        return(
-                        <tr key={elem.region}>
-                            <td>{elem.region}</td>
-                            <td>{elem.cases}</td>
-                            <td>{elem.cases_daily}</td>
-                            <td>{elem.deaths}</td>
-                            <td>{elem.deaths_daily}</td>
-                            <td>{elem.hospitalizations}</td>
-                            <td>{elem.hospitalizations_daily}</td>
-                            <td>{elem.icu}</td>
-                            <td>{elem.icu_daily}</td>
-                            <td>{elem.tests_completed}</td>
-                            <td>{elem.tests_completed_daily}</td>
-                            <td>{elem.vaccine_coverage_dose_1}</td>
-                            <td>{elem.vaccine_coverage_dose_1_daily}</td>
-                            <td>{elem.vaccine_coverage_dose_2}</td>
-                            <td>{elem.vaccine_coverage_dose_2_daily}</td>
-                            <td>{elem.vaccine_coverage_dose_3}</td>
-                            <td>{elem.vaccine_coverage_dose_3_daily}</td>
+                    { // loop over the header rows
+                    headerGroups.map( headerGroup => (
+                        // Apply the header row props
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            { // loop over the headers in each row
+                            headerGroup.headers.map(column => (
+                                // Apply the header cell props
+                                <th {...column.getHeaderProps()}>
+                                    { // Render the header
+                                    column.render('Header')
+                                    }
+                                </th>
+                            ))
+                            }
+                            <th></th>
                         </tr>
+                    ))
+                    }
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {// Loop over the table rows
+                    rows.map(row => {
+                        // Prepare the row for display
+                        prepareRow(row)
+                        return (
+                            // Apply the row props
+                            <tr {...row.getRowProps()}>
+                                {// loop over the rows cells
+                                row.cells.map(cell => {
+                                    // Apply the cell props
+                                    return(
+                                        <td {...cell.getCellProps()}>
+                                            {// Render the cell contents
+                                            cell.render('Cell')
+                                            }
+                                        </td>
+                                    )
+                                })
+                                }
+                            </tr>
                         )
-                        
-                     })}
+                    })
+                    }
                 </tbody>
-                
             </table>
         </div>
     )
